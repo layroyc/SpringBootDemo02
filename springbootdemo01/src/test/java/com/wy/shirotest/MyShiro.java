84a49db163afc59c9002ec4ee4691be881535448
lay <layroyk@163.com>
2021-10-09 12:37:26 +0800

    springboot分页及搜索框

---
+++ springbootdemo01/src/test/java/com/wy/shirotest/MyShiro.java
@@ -0,0 +1,14 @@
+package com.wy.shirotest;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * shiro 的加密 和 认证测试
+ */
+public class MyShiro {
+    //shiro 有 对 明文密码 123456 有 加密功能， 让web网站更加安全
+    @Test
+    public void testCmd5(){
+
+    }
+}
